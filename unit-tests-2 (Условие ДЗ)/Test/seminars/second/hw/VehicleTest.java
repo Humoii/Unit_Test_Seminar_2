fd3a3192b53bf636da1106e72da36aeb40c8f7bb
Сергей <sergeiik123@yandex.ru>
2023-10-24 13:14:10 +0300

    ДЗ_2

---
+++ unit-tests-2 (Условие ДЗ)/Test/seminars/second/hw/VehicleTest.java
@@ -0,0 +1,66 @@
+package seminars.second.hw;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.ClassBasedNavigableIterableAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class VehicleTest {
+
+    Car car;
+    Motorcycle motorcycle;
+
+    @BeforeEach
+    void setUp(){
+//        Подготовка
+        car = new Car("Porshe", "911", 9);
+        motorcycle = new Motorcycle("Yamaha", "XVS950", 5);
+    }
+    @Test
+    public void testCarInstasOfVehicle() {
+//        Проверка утверждения
+        assertTrue(car instanceof Vehicle);
+    }
+    @Test
+    public void testCarFourWheels(){
+//        Проверка утверждения
+//        assertThat(car.getNumWheels()).isEqualTo(4); // todo выдает ошибку нельзя int
+        assertEquals(car.getNumWheels(), 4);
+    }
+    @Test
+    public void testMotorcycle(){
+//        Проверка утверждения
+        assertEquals(motorcycle.getNumWheels(), 2);
+    }
+
+    @Test
+    public  void testCarTestDrive(){
+//           Выполнение
+        car.testDrive();
+//        Проверка утверждения
+        assertEquals(car.getSpeed(), 60);
+    }
+    @Test
+    public void testMotorcycleTestDrive(){
+//        Выполнение
+        motorcycle.testDrive();
+        assertEquals(motorcycle.getSpeed(), 75);
+    }
+    @Test
+    public void testParkCarStopSpeed(){
+//        Выполнение
+        car.testDrive();
+        car.park();
+//        Проверка утверждения
+        assertEquals(car.getSpeed(), 0);
+    }
+    @Test
+    public void testParkMotorcycleStopSpeed(){
+//        Выполнение
+        motorcycle.testDrive();
+        motorcycle.park();
+//        Проверка утверждения
+        assertEquals(motorcycle.getSpeed(), 0);
+    }
+}
